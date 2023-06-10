bc4a5e28a50f710f0d88c589fc82677c97e82c03
Glenn <glenn.heo@adriel.com>
2023-06-10 21:50:16 +0900

    같이하기 UI 및 버튼 작업 완료. 단 아직 같이 게임하기 버튼눌렀을 때 결제창은 아직 구현 안한상태

--- lib/presentation/views/mate/mate_request_together_page.dart
+++ lib/presentation/views/mate/mate_request_together_page.dart
@@ -1,8 +1,11 @@
+import 'package:chemiplay/injection.dart';
+import 'package:chemiplay/presentation/viewmodels/mate_together_viewmodel.dart';
 import 'package:chemiplay/presentation/widgets/app_bar_lead_icon.dart';
 import 'package:chemiplay/presentation/widgets/game_cost.dart';
 import 'package:chemiplay/presentation/widgets/gigi_app_bar.dart';
 import 'package:chemiplay/presentation/widgets/gigi_elevated_three_input_button.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MateRequestTogetherPage extends StatefulWidget {
   const MateRequestTogetherPage({super.key});
@@ -14,76 +17,111 @@ class MateRequestTogetherPage extends StatefulWidget {
 
 class _MateRequestTogetherPageState extends State<MateRequestTogetherPage> {
   final Color backgroundColor = const Color(0xffF5F7FA);
+  MateTogetherViewModel _mateTogetherViewModel = getIt<MateTogetherViewModel>();
+  @override
+  void initState() {
+    super.initState();
+    _mateTogetherViewModel.init(
+      costPerMatch: 1100,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: GigiAppBar(
-        title: '같이하기',
-        leading: AppBarLeadIcon(context: context),
-        backgroundColor: backgroundColor,
-      ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          children: [
-            SizedBox(height: 10),
-            MateInformationCard(),
-            SizedBox(
-              height: 10,
+    return ChangeNotifierProvider<MateTogetherViewModel>(
+      create: (context) => _mateTogetherViewModel,
+      child: Consumer<MateTogetherViewModel>(builder: (context, viewModel, _) {
+        _mateTogetherViewModel = viewModel;
+        return Scaffold(
+          appBar: GigiAppBar(
+            title: '같이하기',
+            leading: AppBarLeadIcon(context: context),
+            backgroundColor: backgroundColor,
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                const MateInformationCard(),
+                const SizedBox(
+                  height: 10,
+                ),
+                GameCard(
+                  costPerMatch: _mateTogetherViewModel.costPerMatch,
+                  theNumberOfTime: _mateTogetherViewModel.theNumberOfTime,
+                  increseTime: _mateTogetherViewModel.increseTime,
+                  decreseTime: _mateTogetherViewModel.decreseTime,
+                  isMinimumTime: _mateTogetherViewModel.isMinimumTime,
+                  isMaximumTime: _mateTogetherViewModel.isMaximumTime,
+                ),
+              ],
             ),
-            GameCard(),
-          ],
-        ),
-      ),
-      backgroundColor: backgroundColor,
-      bottomSheet: Padding(
-        padding: const EdgeInsets.only(
-          left: 20,
-          right: 20,
-          bottom: 30,
-        ),
-        // child: GigiElevatedButton(text: '게임 같이하기'),
-        child: GigiElevatedThreeInputButton(
-          prefixWidget: Container(
-            width: 30,
-            height: 30,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Color(0xff2B2B2B),
+          ),
+          backgroundColor: backgroundColor,
+          bottomSheet: Padding(
+            padding: const EdgeInsets.only(
+              left: 20,
+              right: 20,
+              bottom: 30,
             ),
-            child: const Center(
-              child: Text(
-                '1',
+            child: GigiElevatedThreeInputButton(
+              prefixWidget: Container(
+                width: 30,
+                height: 30,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xff2B2B2B),
+                ),
+                child: Center(
+                  child: Text(
+                    _mateTogetherViewModel.theNumberOfTime.toString(),
+                    style: const TextStyle(
+                      color: Color(0xffFF8066),
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              widget: const Text(
+                '같이 게임하기',
                 style: TextStyle(
-                  color: Color(0xffFF8066),
                   fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xFF2B2B2B),
+                ),
+              ),
+              suffixWidget: Text(
+                "${_mateTogetherViewModel.costPerMatch * _mateTogetherViewModel.theNumberOfTime}코인",
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w900,
+                  color: Color(0xFF2B2B2B),
                 ),
               ),
             ),
           ),
-          widget: const Text(
-            '같이 게임하기',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-              color: Color(0xFF2B2B2B),
-            ),
-          ),
-          suffixWidget: const Text('1100코인',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w900,
-                color: Color(0xFF2B2B2B),
-              )),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
 
 class GameCard extends StatelessWidget {
+  final int costPerMatch;
+  final int theNumberOfTime;
+  final VoidCallback increseTime;
+  final VoidCallback decreseTime;
+  final bool isMinimumTime;
+  final bool isMaximumTime;
   const GameCard({
     super.key,
+    required this.costPerMatch,
+    required this.theNumberOfTime,
+    required this.increseTime,
+    required this.decreseTime,
+    required this.isMinimumTime,
+    required this.isMaximumTime,
   });
 
   @override
@@ -125,23 +163,23 @@ class GameCard extends StatelessWidget {
             const SizedBox(
               height: 12,
             ),
-            const Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Text(
+                const Text(
                   '가격',
                   style: TextStyle(color: Color(0xff6E737A)),
                 ),
                 GameCost(
-                  cost: '1100',
+                  cost: costPerMatch.toString(),
                   unit: '판',
-                  costStyle: TextStyle(
+                  costStyle: const TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.w600,
                     color: Color(0xff262626),
                   ),
-                  unitStyle: TextStyle(
+                  unitStyle: const TextStyle(
                     fontSize: 20,
                     color: Color(0xff6E737A),
                   ),
@@ -163,24 +201,29 @@ class GameCard extends StatelessWidget {
                   width: 90,
                   child: Stack(
                     children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: const Color(0xffCDD3D9),
-                            width: 1,
+                      GestureDetector(
+                        onTap: decreseTime,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: const Color(0xffCDD3D9),
+                              width: 1,
+                            ),
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(5),
+                              bottomLeft: Radius.circular(5),
+                            ),
                           ),
-                          borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(5),
-                            bottomLeft: Radius.circular(5),
-                          ),
-                        ),
-                        width: 30,
-                        height: 30,
-                        alignment: Alignment.center,
-                        child: const Text(
-                          '-',
-                          style: TextStyle(
-                            fontSize: 20,
+                          width: 30,
+                          height: 30,
+                          alignment: Alignment.center,
+                          child: Text(
+                            '-',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: !isMinimumTime
+                                    ? Colors.black
+                                    : const Color(0xffCDD3D9)),
                           ),
                         ),
                       ),
@@ -196,33 +239,38 @@ class GameCard extends StatelessWidget {
                           width: 30,
                           height: 30,
                           alignment: Alignment.center,
-                          child: const Text(
-                            '1',
-                            style: TextStyle(
+                          child: Text(
+                            theNumberOfTime.toString(),
+                            style: const TextStyle(
                                 fontSize: 15, fontWeight: FontWeight.w600),
                           ),
                         ),
                       ),
                       Positioned(
                         right: 0,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: const Color(0xffCDD3D9),
-                              width: 1,
+                        child: GestureDetector(
+                          onTap: increseTime,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: const Color(0xffCDD3D9),
+                                width: 1,
+                              ),
+                              borderRadius: const BorderRadius.only(
+                                topRight: Radius.circular(5),
+                                bottomRight: Radius.circular(5),
+                              ),
                             ),
-                            borderRadius: const BorderRadius.only(
-                              topRight: Radius.circular(5),
-                              bottomRight: Radius.circular(5),
-                            ),
-                          ),
-                          width: 30,
-                          height: 30,
-                          alignment: Alignment.center,
-                          child: const Text(
-                            '+',
-                            style: TextStyle(
-                              fontSize: 20,
+                            width: 30,
+                            height: 30,
+                            alignment: Alignment.center,
+                            child: Text(
+                              '+',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: !isMaximumTime
+                                      ? Colors.black
+                                      : const Color(0xffCDD3D9)),
                             ),
                           ),
                         ),
