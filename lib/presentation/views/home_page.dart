c885095c937f6650fb831ec68c48125ab0935984
Glenn <glenn.heo@adriel.com>
2023-07-22 10:32:24 +0900

    [메이트 프로필] BottomNavigationBar 안보이게 제거

--- lib/presentation/views/home_page.dart
+++ lib/presentation/views/home_page.dart
@@ -48,7 +48,7 @@ class _HomePageState extends State<HomePage> {
     return location == '/mates' ||
         location == '/chats' ||
         location.contains('/feed') ||
-        location.contains('/my');
+        location.contains('/my') && !location.contains('/my/mate');
   }
 
   @override
